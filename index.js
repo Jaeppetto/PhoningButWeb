d995a96b1c8e86cc6c99d832b62196576d3c4fbb
Jaewoong Hwang <noa9925@gmail.com>
2023-02-03 14:32:25 +0900

    2nd commit
    Implement Index page Scroll Event.

---
+++ index.js
@@ -0,0 +1,14 @@
+var phoningVideo = document.getElementById("phoningVideo");
+var frame = 0;
+var vidSize = 0;
+
+window.addEventListener("scroll", () => {
+  //Video Length : 6.967
+  frame = (6.967 / 3000) * scrollY;
+  phoningVideo.currentTime = frame;
+
+  vidSize = (1 / 3000) * scrollY + 0.5;
+  phoningVideo.style.transform = `scale(${vidSize})`;
+});
+
+// scrollY > 2700부터 서서히 투명해지게.
